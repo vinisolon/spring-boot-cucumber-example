3c9174be5b9e8b57ad7c58ebe30890ebb993cb15
Vinicius Solon <vini.97@live.com>
2025-04-05 12:18:31 -0300

    Start project with basic configuration

---
+++ src/main/java/org/vinisolon/cucumber/example/app/SpringBootCucumberExampleApplication.java
@@ -0,0 +1,13 @@
+package org.vinisolon.cucumber.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCucumberExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCucumberExampleApplication.class, args);
+	}
+
+}
